2ab05babd4b0da77cd4a1e106138072672a13a90
hmu1540 <h.mu.1540@gmail.com>
2022-07-27 20:20:22 -0500

    add APP

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -11,6 +11,7 @@
  */
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import MainContainer from './containers/MainContainer.jsx'
 
 class App extends Component {
   constructor(props) {
